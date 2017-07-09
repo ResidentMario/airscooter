06fd8fb715ca151eac8b2faa38442b7b4d40881a
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-09 14:44:04 -0400

    More work on the CLI.

--- datablocks/cli.py
+++ datablocks/cli.py
@@ -1,5 +1,10 @@
 import click
 from .orchestration import *
+from .depositor import Depositor
+from .transform import Transform
+import os
+
+from ast import literal_eval
 
 
 @click.group()
@@ -9,19 +14,47 @@ def cli():
 
 @click.command()
 def init():
-    click.echo('Initialized the database')
     configure(init=True)
+    click.echo('Initialized the database')
 
+
+@click.command()
+def reset():
+    configure()
+    click.echo('Reset the database')
+
+
+@click.command()
+@click.argument('task')
+@click.option('--inputs', help='A filename or list of filenames for data being inputted.')
+@click.option('--outputs', help='A filename or list of filenames for data being outputted.')
+def link(task, inputs, outputs):
+    if outputs is None:
+        raise ValueError("No output filenames provided.")
+
+    _outputs = literal_eval(outputs) if outputs[0] == "[" else [outputs]
+    _inputs = None if inputs is None else literal_eval(inputs) if inputs[0] == "[" else [inputs]
+    task_id = task.split(".")[0]
+
+    if inputs is None:
+        # depositor
+        _task = Depositor(task_id, task, _outputs)
+    else:
+        # transform
+        pass
+
+    if "datablocks.yml" not in os.listdir(".airflow"):
+        graph = []
+    else:
+        graph = deserialize_from_file("./.airflow/datablocks.yml")
+
+    # Write the update to file.
+    # TODO: deal with doubled declarations (e.g. running the same command twice)
+    graph.append(_task)
+    serialize_to_file(graph, "./.airflow/datablocks.yml")
+
+    click.echo(task + "\n" + str(inputs) + "\n" + str(outputs))
+    click.echo(_task)
 
 cli.add_command(init)
-
-# @click.command()
-# # @click.option('--count', default=1, help='Number of greetings.')
-# # @click.option('--name', prompt='Your name',
-# #               help='The person to greet.')
-# def init():
-#     """Simple program that greets NAME for a total of COUNT times."""
-#     for x in range(5):
-#         click.echo('Hello %s!' % x)
-# if __name__ == '__main__':
-#     cli()
+cli.add_command(link)
